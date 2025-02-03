5c5639fb5a6929f90448df3342ccde84ea3fe99d
gtavandrdr <gtavandrdr@gmail.com>
2025-02-03 12:20:45 +0545

    Update Script.py

--- Script.py
+++ Script.py
@@ -19,7 +19,7 @@ Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
     
 ‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/{}>{}</a>
 ‣ ᴍʏ ʙᴇsᴛ ғʀɪᴇɴᴅ : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
-‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/Tv_serial_wala'>ᴛᴇᴄʜ ᴠᴊ</a> 
+‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/HdMovies_SupportBot'>ᴛᴇᴄʜ ᴠᴊ</a> 
 ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a> 
 ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a> 
 ‣ ᴅᴀᴛᴀ ʙᴀsᴇ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a> 
@@ -495,7 +495,7 @@ Aᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs:
 
  எப்படி மற்றும் என்ன செய்ய வேண்டும்
 
- படி 1: இந்த Sharma_movies_bot போட் உங்கள் குழுவை நிர்வாகியாக்குங்கள்
+ படி 1: இந்த HDMoviezBot போட் உங்கள் குழுவை நிர்வாகியாக்குங்கள்
 
  படி 2: உங்கள் இணையதளம் மற்றும் API ஐச் சேர்க்கவும்
 
@@ -522,7 +522,7 @@ Hey <a href='tg://settings'>ᴍʏ ғʀɪᴇɴᴅ</a>
 
  How and what to do
 
- Step 1: Administer this Sharma_movies_bot bot to your group
+ Step 1: Administer this HDMoviezBot bot to your group
 
  Step 2: Add your website and API
 
@@ -549,7 +549,7 @@ Also your tutorial will be Added Your Group..."""
 
  ఎలా మరియు ఏమి చేయాలి
 
- దశ 1: ఈ Sharma_movies_bot బాట్‌ని మీ సమూహానికి నిర్వహించండి
+ దశ 1: ఈ  HDMoviezBot బాట్‌ని మీ సమూహానికి నిర్వహించండి
 
  దశ 2: మీ వెబ్‌సైట్ మరియు APIని జోడించండి
 
@@ -630,7 +630,7 @@ Also your tutorial will be Added Your Group..."""
 
  کیسے اور کیا کرنا ہے۔
 
- مرحلہ 1: اپنے گروپ میں اس Sharma_movies_bot بوٹ کا انتظام کریں۔
+ مرحلہ 1: اپنے گروپ میں اس  HDMoviezBot بوٹ کا انتظام کریں۔
 
  مرحلہ 2: اپنی ویب سائٹ اور API شامل کریں۔
 
@@ -657,7 +657,7 @@ Also your tutorial will be Added Your Group..."""
 
  કેવી રીતે અને શું કરવું
 
- પગલું 1: તમારા જૂથમાં આ Sharma_movies_bot બોટનું સંચાલન કરો
+ પગલું 1: તમારા જૂથમાં આ HDMoviezBot બોટનું સંચાલન કરો
 
  પગલું 2: તમારી વેબસાઇટ અને API ઉમેરો
 
@@ -709,7 +709,7 @@ Also your tutorial will be Added Your Group..."""
 
  কিভাবে এবং কি করতে হবে
 
- ধাপ 1: আপনার গ্রুপে এই Sharma_movies_bot বট পরিচালনা করুন
+ ধাপ 1: আপনার গ্রুপে এই HDMoviezBot বট পরিচালনা করুন
 
  ধাপ 2: আপনার ওয়েবসাইট এবং API যোগ করুন
 
@@ -727,13 +727,13 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/Tv_serial_wala'>VJ</a>
+-Dev 1 [Owner of this bot ]<a href='https://t.me/HdMovies_SupportBot'>HD Moviez</a>
 
--Dev 2 <a href='https://t.me/Tv_serial_wala'>VJ</a>
+-Dev 2 <a href='https://t.me/HdMovies_SupportBot'>HD Moviez</a>
 
--Dev 3 <a href='https://t.me/Tv_serial_wala>VJ Botz</a>
+-Dev 3 <a href='https://t.me/HdMovies_SupportBot'>HD Moviez</a>
 
-- Dev 4 <a href='https://t.me/Tv_serial_wala'>TEAM VJ</a>
+- Dev 4 <a href='https://t.me/HdMovies_SupportBot'>TEAM HD Moviez</a>
 """
 
     RENAME_TXT = """
