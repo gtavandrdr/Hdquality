96d7da4035a445fae87cfa63175323fa50ac53dd
gtavandrdr <gtavandrdr@gmail.com>
2025-02-04 17:44:07 +0545

    Update info.py

--- info.py
+++ info.py
@@ -134,7 +134,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '26c58ac6c7879cf25966a3f6fc999c9c1e3c9315')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/akmoviefile0/18') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/UnpaidMovies/59') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
